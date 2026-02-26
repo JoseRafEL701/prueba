80e91bdcd44a5155bbeb0b9510d52e23a2dc5379
ashley barrios <joserafaelmarenco@gmail.com>
2026-02-26 08:58:53 -0500

    primer intento

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("hola")
-hhhdjsbcuj
+import random
+numero_secreto = random.randint(1,30)
+print("adivina el numero entre el 1 y el 30")
+
+while True:
+    intento = int(input("tu numero: "))
+    if intento == numero_secreto: 
+        print(" ¡Ganaste!" )
+        break
+    elif intento < numero_secreto:
+        print("muy bajo")
+    else:
+        print("muy alto")
+
